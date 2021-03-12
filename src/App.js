05e04554fce8c0595b9085f55f35e3cc686b4f3e
Nasir Uddin <monforingnasir@gmail.com>
2021-03-12 17:31:58 +0600

    successfully added like button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import LikeButton from './components/LikeButton/LikeButton';
 
 function App() {
   return (
     <div className="App">
-      
+      <LikeButton/>
     </div>
   );
 }
--- src/components/LikeButton/LikeButton.js
+++ src/components/LikeButton/LikeButton.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 
 const LikeButton = () => {
+    const [likeColor, setLikeColor] = useState(" ")
+    const [randomUser, setRandomUser] = useState({})
+    useEffect(() => {
+        fetch('https://randomuser.me/api/')
+            .then(res => res.json())
+            .then(data => setRandomUser(data.results[0]))
+    }, [])
+    const handleLike = () => {
+        const color = likeColor ? '' : "primary"
+        setLikeColor(color)
+    }
     return (
         <div>
-            
+            <ThumbUpAltIcon onClick={handleLike} color={likeColor}></ThumbUpAltIcon>
+            <div>
+                <h2>{randomUser.gender}</h2>
+                {/* <h1>Name: {randomUser.name?.first}</h1> */}
+                <h1>Name: {randomUser.name && randomUser.name.first}</h1>
+            </div>
         </div>
     );
 };
